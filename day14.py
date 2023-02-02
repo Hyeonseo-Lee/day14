5c93dc02e278d0a91e59ca367bd02fdc7fd4e5a9
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-02 15:08:29 +0900

    day14 class 09 pokemon game v0.3

--- day14.py
+++ day14.py
@@ -18,6 +18,8 @@ class Pokemon:
         for i in range(len(self.skills)):
             print(f'{i+1} : {self.skills[i]}')
 
+owner = property(get_owner, set_owner)
+
         # for skill in self.skills:
         #     print(f'{skill}')
 
@@ -32,7 +34,7 @@ class Pikachu(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
+        print(f'{self.owner()}의 {self.name}가 {self.skills[idx]} 공격(전기) 시전!')
 
 
 class Ggoboogi(Pokemon):  # inheritance
@@ -42,7 +44,7 @@ class Ggoboogi(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
+        print(f'{self.owner()}의 {self.name}가 {self.skills[idx]} 공격(물) 시전!')
 
     def swim(self):
         print(f'{self.name}가 수영을 합니다')
@@ -55,7 +57,7 @@ class Pairi(Pokemon):  # inheritance
         print(f"{self.name}")
 
     def attack(self, idx):  # override
-        print(f'{self.get_owner()}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
+        print(f'{self.owner()}의 {self.name}가 {self.skills[idx]} 공격(불) 시전!')
 
 
 while True:
